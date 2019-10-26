f17976dce4ded3cad2888a0bff80b395dd499719
TabDeveloper <gugutab@gmail.com>
2019-10-26 12:31:03 -0300

    removed exo player (for now) and set minSDK to 14 again. Using mediaplayer to play video and audio

--- app/src/main/java/com/tabdeveloper/devchallenge/data/model/VideoListModel.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/data/model/VideoListModel.kt
@@ -1,7 +1,10 @@
 package com.tabdeveloper.devchallenge.data.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class VideoListModel(
     @SerializedName("objects") val objects: List<VideoModel>
-)
+) : Parcelable
--- app/src/main/java/com/tabdeveloper/devchallenge/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/ui/player/PlayerActivity.kt
@@ -2,10 +2,13 @@ package com.tabdeveloper.devchallenge.ui.player
 
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.tabdeveloper.devchallenge.R
 import com.tabdeveloper.devchallenge.data.model.VideoModel
+import kotlinx.android.synthetic.main.activity_player.*
 import timber.log.Timber
 
 class PlayerActivity : AppCompatActivity() {
@@ -20,10 +23,40 @@ class PlayerActivity : AppCompatActivity() {
         }
     }
 
+
+    private var audioMediaPlayer: MediaPlayer? = null
+    private var videoModel: VideoModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
-        val videoModel: VideoModel = intent.getParcelableExtra(VIDEO_MODEL)
-        Timber.d("PlayerActivity: $videoModel")
+        videoModel = intent.getParcelableExtra(VIDEO_MODEL)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        videoModel?.let {
+            // audio
+            audioMediaPlayer = MediaPlayer.create(this, Uri.parse(it.audio))
+            audioMediaPlayer?.setOnPreparedListener {
+                Timber.d("prepared")
+                it.start()
+            }
+            audioMediaPlayer?.setOnCompletionListener {
+                activity_player_videoview.stopPlayback()
+            }
+            // video
+            activity_player_videoview.setVideoPath(it.video)
+            activity_player_videoview.start()
+            activity_player_videoview.setOnPreparedListener {
+                it.isLooping = true
+            }
+        }
+    }
+
+    override fun onPause() {
+        audioMediaPlayer?.pause()
+        audioMediaPlayer?.release()
+        super.onPause()
     }
 }
