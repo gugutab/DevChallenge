50660a659771eda2809296db5f8380c9d696bf66
TabDeveloper <gugutab@gmail.com>
2019-10-27 12:57:15 -0300

    handling errors and added swipe to refresh on home

--- app/src/main/java/com/tabdeveloper/devchallenge/data/services/VideoService.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/data/services/VideoService.kt
@@ -6,6 +6,7 @@ import com.tabdeveloper.devchallenge.data.model.VideoModel
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface VideoService {
     @GET("assets.json")
@@ -14,12 +15,19 @@ interface VideoService {
         @Query("token") token: String = AppConfiguration.token
     ): Single<VideoListModel>
 
-    companion object{
-        fun getVideoModelListMock(): Single<VideoListModel>{
-            val videoModel = VideoModel("Title", "https://testdrive-archive.azurewebsites.net/Graphics/VideoFormatSupport/big_buck_bunny_trailer_480p_baseline.mp4",
-                "https://miro.medium.com/max/3000/1*MI686k5sDQrISBM6L8pf5A.jpeg", "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3")
-            val list = VideoListModel(listOf(videoModel, videoModel, videoModel, videoModel, videoModel))
+    companion object {
+        fun getVideoModelListMock(): Single<VideoListModel> {
+            val videoModel = VideoModel(
+                "Title",
+                "https://testdrive-archive.azurewebsites.net/Graphics/VideoFormatSupport/big_buck_bunny_trailer_480p_baseline.mp4",
+                "https://miro.medium.com/max/3000/1*MI686k5sDQrISBM6L8pf5A.jpeg",
+                "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
+            )
+            val list =
+                VideoListModel(listOf(videoModel, videoModel, videoModel, videoModel, videoModel))
             return Single.just(list)
+                .delay(1, TimeUnit.SECONDS)
+//                .timeout(4, TimeUnit.SECONDS)
         }
     }
 }
--- app/src/main/java/com/tabdeveloper/devchallenge/ui/home/HomeActivity.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/ui/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.tabdeveloper.devchallenge.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tabdeveloper.devchallenge.R
 import com.tabdeveloper.devchallenge.data.Module
@@ -17,15 +18,34 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        loadContent()
+        activity_home_swipe_layout.setOnRefreshListener {
+            activity_home_swipe_layout.isRefreshing = false
+            loadContent()
+        }
+        activity_home_error_reload.setOnClickListener {
+            loadContent()
+        }
+    }
 
-//        videoService.getVideoModelList()
+    fun loadContent() {
+        //        videoService.getVideoModelList()
         VideoService.getVideoModelListMock() // todo revert to actual data
             .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                activity_home_loading.isVisible = true
+                activity_home_swipe_layout.isVisible = false
+                activity_home_error.isVisible = false
+            }
             .subscribe({
-                activity_main_recycler.layoutManager = LinearLayoutManager(this)
-                activity_main_recycler.adapter = HomeAdapter(this, it)
+                activity_home_recycler.layoutManager = LinearLayoutManager(this)
+                activity_home_recycler.adapter = HomeAdapter(this, it)
+                activity_home_loading.isVisible = false
+                activity_home_swipe_layout.isVisible = true
             },
                 { throwable: Throwable ->
+                    activity_home_error.isVisible = true
+                    activity_home_loading.isVisible = false
                     Timber.e(throwable)
                 })
     }
