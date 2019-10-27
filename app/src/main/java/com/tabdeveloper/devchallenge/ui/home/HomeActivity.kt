baee8234093dbcf9749d374dd8cbdf3ed56c4183
TabDeveloper <gugutab@gmail.com>
2019-10-27 18:12:14 -0300

    downloading all assets from videoModel

--- app/src/main/java/com/tabdeveloper/devchallenge/ui/home/HomeActivity.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/ui/home/HomeActivity.kt
@@ -7,16 +7,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.tabdeveloper.devchallenge.R
 import com.tabdeveloper.devchallenge.data.Module
 import com.tabdeveloper.devchallenge.data.services.VideoService
-import io.reactivex.Observable
+import com.tabdeveloper.devchallenge.utils.DownloadHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Function
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_home.*
-import okhttp3.ResponseBody
-import retrofit2.Response
 import timber.log.Timber
-import java.io.File
-import java.io.FileOutputStream
 
 class HomeActivity : AppCompatActivity() {
     val videoService = Module.createService()
@@ -35,24 +30,29 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun loadContent() {
-        videoService.downloadFile("https://miro.medium.com/max/3000/1*MI686k5sDQrISBM6L8pf5A.jpeg")
-            .flatMap(Function<Response<ResponseBody>, Observable<File>>() { t ->
-                try {
-                    val file = File(this.filesDir, "image.jpg")
-                    val fileOutputStream = FileOutputStream(file)
-                    fileOutputStream.write(t.body()?.bytes())
-                    fileOutputStream.close()
-                    return@Function Observable.just(file)
-                } catch (ex: Exception) {
-                    return@Function Observable.just(null)
-                }
-            })
-            .doOnSubscribe { Timber.d("download start") }
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                Timber.d("download complete")
-            }
+//        videoService.downloadFile("https://miro.medium.com/max/3000/1*MI686k5sDQrISBM6L8pf5A.jpeg")
+//            .flatMap(Function<Response<ResponseBody>, Observable<File>> { t ->
+//                try {
+//                    val folder = File(filesDir, "test/")
+//                    if(!folder.exists()){
+//                        folder.mkdirs()
+//                    }
+//                    val file = File(folder, "image.jpg")
+//                    val fileOutputStream = FileOutputStream(file)
+//                    fileOutputStream.write(t.body()?.bytes())
+//                    fileOutputStream.close()
+//                    return@Function Observable.just(file)
+//                } catch (ex: Exception) {
+//                    return@Function Observable.just(null)
+//                }
+//            })
+//            .doOnSubscribe { Timber.d("download start") }
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                Timber.d("download complete")
+//            }
+
 //                videoService.getVideoModelList()
         VideoService.getVideoModelListMock() // todo revert to actual data
             .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
@@ -62,6 +62,11 @@ class HomeActivity : AppCompatActivity() {
                 activity_home_error.isVisible = false
             }
             .subscribe({
+
+                DownloadHelper.downloadVideoModel(this, it.objects.first()!!, videoService).subscribe {
+                    Timber.d("download DONE!")
+                }
+
                 activity_home_recycler.layoutManager = LinearLayoutManager(this)
                 activity_home_recycler.adapter = HomeAdapter(this, it)
                 activity_home_loading.isVisible = false
---
+++ app/src/main/java/com/tabdeveloper/devchallenge/utils/DownloadHelper.kt
@@ -0,0 +1,60 @@
+package com.tabdeveloper.devchallenge.utils
+
+import android.content.Context
+import com.tabdeveloper.devchallenge.data.model.VideoModel
+import com.tabdeveloper.devchallenge.data.services.VideoService
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import okhttp3.ResponseBody
+import retrofit2.Response
+import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
+
+class DownloadHelper {
+    companion object {
+        fun downloadVideoModel(
+            context: Context,
+            videoModel: VideoModel,
+            videoService: VideoService
+        ): Observable<File> {
+            //create folder
+            val folder = File(context.filesDir, "${videoModel.name.getIntFromString()}/")
+            if (!folder.exists()) {
+                folder.mkdirs()
+            }
+            //
+            return Observable.concat<File>(
+                downloadFile(videoService, videoModel.audio, folder, "audio.mp3"),
+                downloadFile(videoService, videoModel.image, folder, "image.jpg"),
+                downloadFile(videoService, videoModel.video, folder, "video.mp4")
+            )
+
+        }
+
+        fun downloadFile(
+            videoService: VideoService,
+            url: String,
+            path: File,
+            fileName: String
+        ): Observable<File> {
+            return videoService.downloadFile(url)
+                .flatMap(io.reactivex.functions.Function<Response<ResponseBody>, Observable<File>> { t ->
+                    try {
+                        val file = File(path, fileName)
+                        val fileOutputStream = FileOutputStream(file)
+                        fileOutputStream.write(t.body()?.bytes())
+                        fileOutputStream.close()
+                        return@Function Observable.just(file)
+                    } catch (ex: Exception) {
+                        throw ex
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { Timber.d("download start: $fileName") }
+                .doOnComplete { Timber.d("download done: $fileName") }
+        }
+    }
+}
