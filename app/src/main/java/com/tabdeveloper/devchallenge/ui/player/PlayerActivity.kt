babea491e54e3697e4ecd9582cf5e86a4016cda0
TabDeveloper <gugutab@gmail.com>
2019-10-28 00:10:03 -0300

    changed implementation to exoplayer and bumped minSDK

--- app/src/main/java/com/tabdeveloper/devchallenge/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/ui/player/PlayerActivity.kt
@@ -3,22 +3,24 @@ package com.tabdeveloper.devchallenge.ui.player
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.graphics.drawable.Drawable
 import android.media.MediaPlayer
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
-import com.tabdeveloper.devchallenge.R
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import com.tabdeveloper.devchallenge.data.model.VideoListModel
 import com.tabdeveloper.devchallenge.data.model.VideoModel
 import com.tabdeveloper.devchallenge.utils.DownloadHelper
 import kotlinx.android.synthetic.main.activity_player.*
 import timber.log.Timber
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -42,6 +44,7 @@ class PlayerActivity : AppCompatActivity() {
     private var videoModel: VideoModel? = null
     private var videoModelDownloaded: VideoModel? = null
     private var currentPosition = 0
+    private var exoplayer: SimpleExoPlayer? = null
     var audioPrepared: Boolean = false
         set(value) {
             field = value
@@ -60,8 +63,12 @@ class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        setContentView(com.tabdeveloper.devchallenge.R.layout.activity_player)
         videoListModel = intent.getParcelableExtra(VIDEO_LIST_MODEL)
+        exoplayer = ExoPlayerFactory.newSimpleInstance(this)
+        activity_player_videoview.player = exoplayer
+        activity_player_videoview.hideController()
+
     }
 
     override fun onResume() {
@@ -85,10 +92,10 @@ class PlayerActivity : AppCompatActivity() {
         activity_player_play_pause_button.setOnClickListener {
             if (audioMediaPlayer != null && audioMediaPlayer!!.isPlaying) {
                 audioMediaPlayer?.pause()
-                activity_player_videoview.pause()
+                exoplayer?.playWhenReady = false
             } else {
                 audioMediaPlayer?.start()
-                activity_player_videoview.start()
+                exoplayer?.playWhenReady = true
             }
             setupPlayPauseButton()
         }
@@ -117,17 +124,22 @@ class PlayerActivity : AppCompatActivity() {
             activity_player_previous_button.setColorFilter(
                 ContextCompat.getColor(
                     this,
-                    R.color.black
+                    com.tabdeveloper.devchallenge.R.color.black
                 )
             )
-            activity_player_next_button.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            activity_player_next_button.setColorFilter(
+                ContextCompat.getColor(
+                    this,
+                    com.tabdeveloper.devchallenge.R.color.black
+                )
+            )
             when (currentPosition) {
                 0 -> {
                     activity_player_previous_button.isEnabled = false
                     activity_player_previous_button.setColorFilter(
                         ContextCompat.getColor(
                             this,
-                            R.color.gray
+                            com.tabdeveloper.devchallenge.R.color.gray
                         )
                     )
                 }
@@ -136,7 +148,7 @@ class PlayerActivity : AppCompatActivity() {
                     activity_player_next_button.setColorFilter(
                         ContextCompat.getColor(
                             this,
-                            R.color.gray
+                            com.tabdeveloper.devchallenge.R.color.gray
                         )
                     )
                 }
@@ -146,9 +158,9 @@ class PlayerActivity : AppCompatActivity() {
 
     private fun setupPlayPauseButton() {
         if (audioMediaPlayer != null && audioMediaPlayer!!.isPlaying) {
-            activity_player_play_pause_button.setImageResource(R.drawable.ic_pause_circle_outline_black_72dp)
+            activity_player_play_pause_button.setImageResource(com.tabdeveloper.devchallenge.R.drawable.ic_pause_circle_outline_black_72dp)
         } else {
-            activity_player_play_pause_button.setImageResource(R.drawable.ic_play_circle_outline_black_72dp)
+            activity_player_play_pause_button.setImageResource(com.tabdeveloper.devchallenge.R.drawable.ic_play_circle_outline_black_72dp)
         }
     }
 
@@ -160,47 +172,59 @@ class PlayerActivity : AppCompatActivity() {
         activity_player_next_button.isVisible = false
         activity_player_previous_button.isVisible = false
         audioMediaPlayer?.release()
-        activity_player_videoview.stopPlayback()
+//        activity_player_videoview.stopPlayback()
+        exoplayer?.stop(true)
         //
         videoModel?.let {
             activity_player_loading.isVisible = true
-            activity_player_videoview.setBackgroundResource(R.drawable.dark_background)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                Glide.with(this).asDrawable().load(videoModelDownloaded?.image ?: it.image)
-                    .into(object : CustomTarget<Drawable>() {
-                        override fun onLoadCleared(placeholder: Drawable?) {
-                            //do nothing
-                        }
 
-                        override fun onResourceReady(
-                            resource: Drawable,
-                            transition: Transition<in Drawable>?
-                        ) {
-                            activity_player_videoview.background = resource
-                        }
-                    })
-            }
+            //TODO check artwork bugs
+//            activity_player_videoview.defaultArtwork =
+//                ContextCompat.getDrawable(this, R.drawable.dark_background)
+//            activity_player_videoview.setBackgroundResource(com.tabdeveloper.devchallenge.R.drawable.dark_background)
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+//                Glide.with(this).asDrawable().load(videoModelDownloaded?.image ?: it.image)
+//                    .into(object : CustomTarget<Drawable>() {
+//                        override fun onLoadCleared(placeholder: Drawable?) {
+//                            //do nothing
+//                        }
+//
+//                        override fun onResourceReady(
+//                            resource: Drawable,
+//                            transition: Transition<in Drawable>?
+//                        ) {
+//                            activity_player_videoview.defaultArtwork = resource
+//                        }
+//                    })
+//            }
+
             // video
-            activity_player_videoview.setVideoPath(videoModelDownloaded?.video ?: it.video)
-            activity_player_videoview.setOnPreparedListener {
-                Timber.d("video prepared")
-                videoPrepared = true
-                it.isLooping = true
-                it.setVolume(0f, 0f)
-                it.setOnInfoListener { mp, what, extra ->
-                    when (what) {
-                        MediaPlayer.MEDIA_INFO_BUFFERING_START -> {
-                            Timber.d("setOnInfoListener MEDIA_INFO_BUFFERING_START")
-                            activity_player_loading.isVisible = true
-                        }
-                        MediaPlayer.MEDIA_INFO_BUFFERING_END -> {
-                            Timber.d("setOnInfoListener MEDIA_INFO_BUFFERING_END")
+            val dataSourceFactory = DefaultDataSourceFactory(
+                this,
+                Util.getUserAgent(this, "DevChallenge")
+            )
+            val videoSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(videoModelDownloaded?.video ?: it.video))
+            // Prepare the player with the source.
+            exoplayer?.playWhenReady = true
+            exoplayer?.prepare(videoSource)
+            exoplayer?.volume = 0f
+            exoplayer?.repeatMode = Player.REPEAT_MODE_ALL
+
+            exoplayer?.addListener(object : Player.EventListener {
+                override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                    when (playbackState) {
+                        Player.STATE_BUFFERING -> activity_player_loading.isVisible = true
+                        Player.STATE_READY -> {
                             activity_player_loading.isVisible = false
+                            if (!videoPrepared) {
+                                videoPrepared = true
+                            }
                         }
                     }
-                    true
+                    super.onPlayerStateChanged(playWhenReady, playbackState)
                 }
-            }
+            })
             // audio
             audioMediaPlayer = MediaPlayer()
             audioMediaPlayer?.setDataSource(videoModelDownloaded?.audio ?: it.audio)
@@ -210,8 +234,8 @@ class PlayerActivity : AppCompatActivity() {
                 audioPrepared = true
             }
             audioMediaPlayer?.setOnCompletionListener {
-                activity_player_videoview.stopPlayback()
-                activity_player_play_pause_button.setImageResource(R.drawable.ic_replay_black_72dp)
+                exoplayer?.stop(true)
+                activity_player_play_pause_button.setImageResource(com.tabdeveloper.devchallenge.R.drawable.ic_replay_black_72dp)
                 activity_player_play_pause_button.setOnClickListener {
                     changeCurrentPosition(currentPosition)
                 }
@@ -222,19 +246,20 @@ class PlayerActivity : AppCompatActivity() {
     fun startPlayback() {
         Timber.d("startPlayback")
         activity_player_videoview.setBackgroundResource(0)
-        activity_player_videoview.start()
+        exoplayer?.playWhenReady = true
         activity_player_play_pause_button.isVisible = true
         setupButtons()
-        setupPlayPauseButton()
         activity_player_loading.isVisible = false
         audioMediaPlayer?.start()
+        setupPlayPauseButton()
+
     }
 
     override fun onPause() {
         audioMediaPlayer?.stop()
         audioMediaPlayer?.reset()
         audioMediaPlayer?.release()
-        activity_player_videoview?.stopPlayback()
+        exoplayer?.stop(true)
         super.onPause()
     }
 }
