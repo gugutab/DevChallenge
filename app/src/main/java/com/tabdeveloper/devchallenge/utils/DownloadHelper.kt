6d62e7afe7de7a3f165d947db3a59ee7d05e5015
TabDeveloper <gugutab@gmail.com>
2019-10-27 22:46:48 -0300

    added dialog and cancel download button

--- app/src/main/java/com/tabdeveloper/devchallenge/utils/DownloadHelper.kt
+++ app/src/main/java/com/tabdeveloper/devchallenge/utils/DownloadHelper.kt
@@ -1,6 +1,9 @@
 package com.tabdeveloper.devchallenge.utils
 
+import android.app.AlertDialog
 import android.content.Context
+import android.view.LayoutInflater
+import com.tabdeveloper.devchallenge.R
 import com.tabdeveloper.devchallenge.data.model.VideoModel
 import com.tabdeveloper.devchallenge.data.services.VideoService
 import io.reactivex.Observable
@@ -45,26 +48,35 @@ class DownloadHelper {
             videoModel: VideoModel,
             videoService: VideoService
         ): Observable<File> {
+            val downloadDialog = displayDownloadDialog(context)
             //create folder
             val folder = File(context.filesDir, "${videoModel.name.getIntFromString()}/")
             if (!folder.exists()) {
                 folder.mkdirs()
             }
             //
-            return Observable.concat(
+            val observable = Observable.concat(
                 downloadFile(videoService, videoModel.audio, folder, audioFileName),
                 downloadFile(videoService, videoModel.image, folder, imageFileName),
                 downloadFile(videoService, videoModel.video, folder, videoFileName)
-            ).doOnComplete {
-                val completeFile = File(folder, downloadCompleteFileName)
-                val stream = FileOutputStream(completeFile)
-                try {
-                    stream.write("done".toByteArray())
-                } finally {
-                    stream.close()
+            )
+                .doOnSubscribe { disposable ->
+                    downloadDialog.setOnDismissListener {
+                        if(!disposable.isDisposed){
+                            disposable.dispose()
+                        }
+                    }
+                    downloadDialog.show()
                 }
-            }
-
+                .doOnComplete {
+                    downloadDialog.dismiss()
+                    val completeFile = File(folder, downloadCompleteFileName)
+                    val stream = FileOutputStream(completeFile)
+                    stream.use {
+                        it.write("done".toByteArray())
+                    }
+                }
+            return observable
         }
 
         fun downloadFile(
@@ -87,8 +99,40 @@ class DownloadHelper {
                 })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { Timber.d("download start: $fileName") }
+                .doOnSubscribe {
+                    Timber.d("download start: $fileName")
+                }
                 .doOnComplete { Timber.d("download done: $fileName") }
+        }
+
+        private fun displayDownloadDialog(context: Context): AlertDialog {
+            val customLayout = LayoutInflater.from(context).inflate(R.layout.dialog_download, null)
+
+            return AlertDialog.Builder(context)
+                .setTitle("Downloading")
+                .setCancelable(false)
+                .setView(customLayout)
+                .setNeutralButton(R.string.download_dialog_cancel) { dialog, _ ->
+                    dialog.cancel()
+                }
+                .create()
+//                .setView(createdView)
+//                .setPositiveButton("Save") { _, _ ->
+//                    val title = titleField.text.toString()
+//                    val description = descriptionField.text.toString()
+//                    val note = ContactsContract.CommonDataKinds.Note(
+//                        title = title,
+//                        description = description
+//                    )
+//                    //logo antes de executar o insert, a HOF é executada
+//                    preExecute()
+//                    NoteWebClient().insert(note, {
+//                        created(it)
+//                    }, {
+//                        Toast.makeText(context, "Falha ao salvar nota", Toast.LENGTH_LONG).show()
+//                    })
+//                }
+//                .show()
         }
     }
 }
